437daca565974ccf3e2f588f37871fb9c0b19498
Kronnos LAN <korozco@lean-tech.io>
2022-03-04 07:15:58 -0500

    other commit

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -101,8 +101,8 @@
                             </div>
                             <div class="flex-1 text-right md:text-center">
                                 <h2 class="font-bold uppercase text-gray-600">NO ESTÁN CENSO</h2>
-                                <p class="font-bold text-3xl">0 <span class="text-red-500"><i
-                                            class="fas fa-caret-up"></i></span>{{ $noestan }}</p>
+                                <p class="font-bold text-3xl">{{ $noestan }} <span class="text-red-500"><i
+                                            class="fas fa-caret-up"></i></span></p>
                             </div>
                         </div>
                     </div>
@@ -111,7 +111,7 @@
             </div>
 
 
-            <div class="flex flex-row flex-wrap flex-grow mt-2">
+            {{-- <div class="flex flex-row flex-wrap flex-grow mt-2">
 
                 <div class="w-full md:w-1/2 xl:w-1/2 p-6">
                     <!--Graph Card-->
@@ -168,11 +168,11 @@
                         </div>
                     </div>
                     <!--/Graph Card-->
-                </div>
+                </div> --}}
 
 
 
-                {{-- <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+            {{-- <div class="w-full md:w-1/2 xl:w-1/3 p-6">
                 <!--Advert Card-->
                 <div class="bg-white border-transparent rounded-lg shadow-xl">
                     <div class="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
@@ -188,7 +188,7 @@
                 </div>
                 <!--/Advert Card-->
             </div> --}}
-            </div>
+            {{-- </div> --}}
         </div>
     </section>
 </div>
